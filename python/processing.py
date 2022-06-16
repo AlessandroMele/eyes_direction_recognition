e02f0f694fc1ee81d6bf6846c58a7db1485fecc6
Alessandro Mele <amele145@gmail.com>
2022-06-16 17:48:18 +0200

    prova

--- python/processing.py
+++ python/processing.py
@@ -17,15 +17,13 @@ new_dim = (100, 100)
 #custom size
 #new_dim = ( , )
 
-ft.data_augmentation(source_path, source_path)
-ft.face_mesh(new_dim, source_path, source_path)
 """
 STEP 1
 data augmentation offline, making:
     - rotations of -5 and 5 degrees;
     - warping perspective by a delta factor of 80
     - !!! color curve has been applied using a GIMP module called BIMP !!!
-
+"""
 ft.data_augmentation(source_path, data_aug_path, folder_class = "/high_center/")
 ft.data_augmentation(source_path, data_aug_path, folder_class = "/high_right/")
 ft.data_augmentation(source_path, data_aug_path, folder_class = "/high_left/")
@@ -37,8 +35,10 @@ ft.data_augmentation(source_path, data_aug_path, folder_class = "/middle_right/"
 ft.data_augmentation(source_path, data_aug_path, folder_class = "/middle_left/")
 print("Data augmentation: completed!")
 
+"""
 STEP 2
 Applying facemesh modules for cutting eyes using bounding boxes, then resizing by custom size
+"""
 ft.face_mesh(new_dim, data_aug_path, face_mesh_path, folder_class = "/high_center/")
 ft.face_mesh(new_dim, data_aug_path, face_mesh_path, folder_class = "/high_right/")
 ft.face_mesh(new_dim, data_aug_path, face_mesh_path, folder_class = "/high_left/")
@@ -49,4 +49,3 @@ ft.face_mesh(new_dim, data_aug_path, face_mesh_path, folder_class = "/middle_cen
 ft.face_mesh(new_dim, data_aug_path, face_mesh_path, folder_class = "/middle_right/")
 ft.face_mesh(new_dim, data_aug_path, face_mesh_path, folder_class = "/middle_left/")
 print("face mesh: completed!")
-"""
